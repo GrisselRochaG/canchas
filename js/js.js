81aff66f599effce80f5fe8cfdc2208b705d26e6
Grissel Rocha <grissel@laboratoria.mx>
2017-04-11 01:26:44 -0500

    canchas soccer y basquet

---
+++ js/js.js
@@ -0,0 +1,9 @@
+function mostrarBasquet(){
+	document.getElementById('soccer').style.display = 'none';
+	document.getElementById('basquet').style.display = 'block';
+}
+
+function mostrarSoccer(){
+	document.getElementById('basquet').style.display = 'none';
+	document.getElementById('soccer').style.display = 'block';
+}
